125746b62b50ddd3322fabc20994166906a3f00b
Sarah <hayes.sc@gmail.com>
2018-03-05 06:44:47 -0500

    Starting to set up database

--- project.py
+++ project.py
@@ -1,13 +1,19 @@
 import csv
 import requests
+import sqlite3
+
+db = sqlite3.connect('mydb.sqlite')
+cur = db.cursor()
+cur.execute('''CREATE TABLE License_Data (
+    ID varchar(10),
+    LicenseID varchar(10),
+    Description; ''' )
 
 CSV_URL = "https://data.louisvilleky.gov/sites/default/files/LocationBasedLicenseData_1.csv"
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-
-    decoded_content = download.content.decode('utf-8')
-
-    cr = csv.reader(decoded_content.splitlines(), delimiter='\t')
-    my_list = list(cr)
-    for row in my_list:
-        print(row)
+response = requests.get(CSV_URL)
+if response.status_code != 200:
+    print("Failed to get data:", response.status_code)
+else:
+    wrapper = csv.reader(response.text.strip().split('\n'))
+    for record in wrapper:
+        print(record)
