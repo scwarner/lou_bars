bf5b7e085cb6f23df2d095bed5bc64ae48eec773
Sarah <hayes.sc@gmail.com>
2018-03-12 19:25:38 -0400

    More work on querying data to pull out information I need for bar chart

--- project.py
+++ project.py
@@ -2,14 +2,15 @@ import csv
 import requests
 import sqlite3
 
-ABC_data = requests.get('https://data.louisvilleky.gov/sites/default/files/LocationBasedLicenseData_3.csv')
-raw_text = ABC_data.text
+#ABC_data = requests.get('https://data.louisvilleky.gov/sites/default/files/LocationBasedLicenseData_3.csv')
+#raw_text = ABC_data.text
 
-license_list = raw_text.split('\r')
-formatted_list = [r.split(',') for r in license_list] 
-for row in formatted_list:
-    del row[5:9]
-print(formatted_list[3])
+#license_list = raw_text.split('\r')
+#formatted_list = [r.split(',') for r in license_list] 
+#[del r[5:9] for r in formatted_list]
+#for row in formatted_list:
+ #   del row[5:9]
+#print(formatted_list[3])
 
 db = sqlite3.connect('mydb.sqlite')
 cursor = db.cursor()
@@ -20,10 +21,6 @@ cursor.execute('''CREATE TABLE License_Data (
     Description varchar(100),
     License_Name varchar(100),
     Location varchar(100),
-    Street_number varchar(100),
-    Prefix varchar(10),
-    Street_name varchar(100),
-    Suffix varchar(10),
     Longitude varchar(100),
     Latitude varchar(100),
     Expiration varchar(100),
@@ -44,7 +41,7 @@ cursor.execute('''CREATE TABLE License_Data (
     ); 
     ''' )
 
-CSV_URL = "https://data.louisvilleky.gov/sites/default/files/LocationBasedLicenseData_3.csv"
+CSV_URL = "https://data.louisvilleky.gov/sites/default/files/LocationBasedLicenseData_1.csv"
 response = requests.get(CSV_URL)
 if response.status_code != 200:
     print("Failed to get data:", response.status_code)
@@ -52,19 +49,20 @@ else:
     wrapper = csv.reader(response.text.strip().split('\n'))
     headerline = True
     for record in wrapper:
+        del record[5:9]
         if headerline:
             headerline = False
         else:
-            cursor.execute('''INSERT INTO License_Data(DataID, LicenseID, Description, License_Name, Location, Street_number, Prefix, Street_name, Suffix, Longitude, Latitude, Expiration, SubType, SubDescription, IssueDate, ZIP_Code, District, Neighborhood, Zoning, EndorsementType, EndorsementTypeDescription, EndorsementStatusDescription, EndorsementIssuedDate, AGENCY, GPSX, GPSY)
-            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (record))
+            cursor.execute('''INSERT INTO License_Data(DataID, LicenseID, Description, License_Name, Location, Longitude, Latitude, Expiration, SubType, SubDescription, IssueDate, ZIP_Code, District, Neighborhood, Zoning, EndorsementType, EndorsementTypeDescription, EndorsementStatusDescription, EndorsementIssuedDate, AGENCY, GPSX, GPSY)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (record))
             db.commit()
 
-num_bars = cursor.execute("SELECT COUNT(DISTINCT License_Name) FROM License_Data WHERE SubDescription = 'Bar';")
-zip_list = cursor.execute("SELECT DISTINCT ZIP_Code FROM License_Data;")
-zip_count = cursor.execute("SELECT COUNT(DISTINCT ZIP_Code) FROM License_Data;")
+#num_bars = cursor.execute("SELECT COUNT(DISTINCT License_Name) FROM License_Data WHERE SubDescription = 'Bar';")
+bar_per_zip = cursor.execute("SELECT COUNT(DISTINCT Location), ZIP_Code FROM License_Data WHERE SubDescription = 'Bar' GROUP BY ZIP_Code;")
+#zip_list = cursor.execute("SELECT DISTINCT ZIP_Code FROM License_Data ORDER BY ZIP_Code ASC;")
+#zip_count = cursor.execute("SELECT COUNT(DISTINCT ZIP_Code) FROM License_Data;")
+#print(zip_list.fetchall())
+#print(bar_per_zip.fetchall())
 
-#for row in cursor.execute("SELECT DISTINCT License_Name, ZIP_Code FROM License_Data WHERE SubDescription = 'Bar' LIMIT 50;"):
- #   print(row)
-
-# for row in cursor.execute("SELECT * FROM License_Data WHERE SubDescription = 'Bar' LIMIT 50;"):
+#for row in bar_per_zip:
 #    print(row)
